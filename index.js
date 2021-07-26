8175e790288f0fd6e345c493f09acf13b85d7b1c
Priyanshu <prynsu@yahoo.com>
2021-07-26 16:26:16 +0530

    Quizzes Route

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Hello! This is Quiziker API.')
+const express = require('express');
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+const app = express();
+app.use(bodyParser.json())
+
+app.use(cors())
+
+// DB connect
+const initializeDBConnect = require("./db/db.connect.js")
+initializeDBConnect()
+
+// Middlewares
+const errorHandler404 = require("./middlewares/errorHandler404.middleware.js")
+const errorHandler500 = require("./middlewares/errorHandler500.middleware.js")
+
+// Routers
+const quizzes = require("./routers/quizzes.v1.router.js")
+
+app.use("/quizzes", quizzes)
+
+app.get('/', (req, res) => {
+  res.send("Welcome to Quiziker's Rest API!")
+});
+
+app.use(errorHandler404)
+app.use(errorHandler500)
+
+const port = 8000
+
+app.listen(process.env.PORT || port, () => {
+  console.log('server started on port 8000');
+});
